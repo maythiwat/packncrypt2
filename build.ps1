532eef86788d094f79f026c5120ea801968e4bf7
Maythiwat Chomchuen <maythiwat.chom@gmail.com>
2024-09-17 23:11:32 +0700

    chore: rename main.exe to app.exe

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ mkdir "./bin" > $null
 
 node --experimental-sea-config "./sea-config.json"
 
-node -e "require('fs').copyFileSync(process.execPath, './bin/main.exe')" 
-signtool remove /s "./bin/main.exe"
+node -e "require('fs').copyFileSync(process.execPath, './bin/app.exe')" 
+signtool remove /s "./bin/app.exe"
 
-npx postject "./bin/main.exe" NODE_SEA_BLOB "./bin/sea-prep.blob" --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 
+npx postject "./bin/app.exe" NODE_SEA_BLOB "./bin/sea-prep.blob" --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 
